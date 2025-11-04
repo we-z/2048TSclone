993719d664e251845360fe9e1c5e69ce890ba008
we-z <yousefsalem00@gmail.com>
2025-11-04 15:09:14 -0800

    dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.scss";
 
 import { animationDuration, gridGap } from "./config";
@@ -6,8 +6,24 @@ import Header from "./components/Header";
 import Board from "./components/Board";
 import Info from "./components/Info";
 import BoardSizePicker from "./components/BoardSizePicker";
+import ThemeToggle from "./components/ThemeToggle";
 
 const App: React.FC = () => {
+  const [isDark, setIsDark] = useState(() => {
+    const savedTheme = localStorage.getItem("theme");
+    return savedTheme ? savedTheme === "dark" : true; // default to dark mode
+  });
+
+  useEffect(() => {
+    const theme = isDark ? "dark" : "light";
+    document.documentElement.setAttribute("data-theme", theme);
+    localStorage.setItem("theme", theme);
+  }, [isDark]);
+
+  const toggleTheme = () => {
+    setIsDark(!isDark);
+  };
+
   return (
     <div
       className="app"
@@ -18,6 +34,7 @@ const App: React.FC = () => {
         } as any
       }
     >
+      <ThemeToggle isDark={isDark} onToggle={toggleTheme} />
       <div className="page">
         <Header />
         <Board />
