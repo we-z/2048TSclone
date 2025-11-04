186162d71d5c4d9e158229cdc41e923fc04b3c43
we-z <yousefsalem00@gmail.com>
2025-11-04 14:30:10 -0800

    default board size 3

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@
 export const animationDuration = 100;
 
 /** Default board size. */
-export const defaultBoardSize = 4;
+export const defaultBoardSize = 3;
 
 /** A list of supported board sizes. */
 export const supportedBoardSizes = [3, 4, 5, 6];
@@ -11,4 +11,4 @@ export const supportedBoardSizes = [3, 4, 5, 6];
 export const victoryTileValue = 2048;
 
 /** Grid gap (used for animations). */
-export const gridGap = '1rem';
+export const gridGap = "1rem";
